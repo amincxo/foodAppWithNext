948af0ac6a480802dd65a45633771c07c790178c
unknown <amincxo@gmail.com>
2025-04-25 16:04:25 +0330

    show data in menu

--- components/templates/MenuPage.js
+++ components/templates/MenuPage.js
@@ -2,9 +2,15 @@ import React from 'react'
 import styles from './MenuPage.module.css'
 
 function MenuPage({data}) {
-    console.log(data)
   return (
-    <div>MenuPage</div>
+    <div className={styles.container} >
+        <h2>Menu</h2>
+        <div className={styles.subContainer} >
+            {
+                data.map(food => (<p key={food.id} > {food.name} </p>))
+            }
+        </div>
+    </div>
   )
 }
 
