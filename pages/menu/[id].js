6915127d7f4480899ef708d70e895fcc7f3a7cc9
unknown <amincxo@gmail.com>
2025-04-25 20:37:04 +0330

    get data in daynamic router

--- pages/menu/[id].js
+++ pages/menu/[id].js
@@ -1,6 +1,12 @@
+import { useRouter } from 'next/router';
 import React from 'react'
 
-function Details() {
+function Details({data}) {
+    const router = useRouter();
+    console.log(data)
+    if(router.isFallback) {
+        return <h2>Loading page ...</h2>
+    }
   return (
     <div>Details</div>
   )
@@ -12,7 +18,7 @@ export async function getStaticPaths() {
     const res = await fetch('http://localhost:4000/data');
     const json = await res.json();
     const data = json.slice(0 , 10 );
-
+    
     const paths = data.map(food=> ({
         params: {id: food.id.toString()},
     }) )
@@ -20,5 +26,21 @@ export async function getStaticPaths() {
     return {
         paths,
         fallback: true,
+    };
+}
+
+export async function getStaticProps(context) {
+    const {params : {id},} = context;
+    const res = await fetch(`http://localhost:4000/data/${id}`);
+    const data = await res.json();
+    if(!data.id) {
+        return {
+            notFound: true,
+        }
     }
+
+    return {
+        props : {data},
+        revalidate: 10,
+    };
 }
