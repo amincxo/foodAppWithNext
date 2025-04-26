512b1d9f873dc2274e778ea283c15eedf01ef594
unknown <amincxo@gmail.com>
2025-04-26 18:30:34 +0330

    debug and cate gory complate and env file are done

--- pages/menu/[id].js
+++ pages/menu/[id].js
@@ -16,7 +16,7 @@ function Details({data}) {
 export default Details
 
 export async function getStaticPaths() {
-    const res = await fetch('http://localhost:4000/data');
+    const res = await fetch(`${process.env.BASE_URL}/data`);
     const json = await res.json();
     const data = json.slice(0 , 10 );
     
@@ -32,7 +32,7 @@ export async function getStaticPaths() {
 
 export async function getStaticProps(context) {
     const {params : {id},} = context;
-    const res = await fetch(`http://localhost:4000/data/${id}`);
+    const res = await fetch(`${process.env.BASE_URL}/data/${id}`);
     const data = await res.json();
     if(!data.id) {
         return {
@@ -42,6 +42,6 @@ export async function getStaticProps(context) {
 
     return {
         props : {data},
-        revalidate: 10,
+        revalidate: +process.env.REVALIDATE,
     };
 }
